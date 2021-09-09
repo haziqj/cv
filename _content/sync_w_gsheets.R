746dd654e62004c1db96f9d76fead888c8008a49
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2021-09-09 19:55:01 +0800

    Almost final version of CV

--- _content/sync_w_gsheets.R
+++ _content/sync_w_gsheets.R
@@ -5,7 +5,7 @@ gs4_deauth()
 sheets <- c("cv_entries", "publications")
 
 store_sheet_in_csv <- function(sheet_name){
-  read_sheet("https://docs.google.com/spreadsheets/d/1ta71CAGkcLqm-W1UdVRA_JJSddWV2TsrRZsCnQlmOis/edit?usp=sharing",
+  read_sheet("https://docs.google.com/spreadsheets/d/1wPqqaIK70DcUNOuOB82n7JYGwW-nySkqsEkSoJxJ9B4/edit?usp=sharing",
              sheet = sheet_name, col_types = c("c")) %>% 
     write_csv(here::here(str_c("_content/", sheet_name, ".csv")))
 }
--- index.md
+++
@@ -1,5 +0,0 @@
-# Test
-
-- [CV (`self_contained: false`)](cv.html)
-- [CV (`self_contained: true`)](cv_self_contained.html)
-- [ulyngs cv](one-column-paged-academic.html)
